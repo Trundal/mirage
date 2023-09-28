332a386671c866441031fd1bb17e0d9a14f2330d
Korey Powell <korey.powell@kayakomat.com>
2023-09-28 15:59:30 +0200

    added favicon

--- pages/impressum.tsx
+++ pages/impressum.tsx
@@ -11,7 +11,9 @@ export default function Impressum() {
           content="MIRAGE VIRTUAL ART DEPARTMENT GMBH - IMPRESSUM"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.webmanifest" />
       </Head>
       <ImpressumPage />
     </>
