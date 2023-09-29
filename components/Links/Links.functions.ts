c79b2581aabecc16dc871a02c1134f2fab2fb9bb
andrehasenoehrl <mail@mirage.gmbh>
2023-09-29 12:31:38 +0200

    ticktok link

--- components/Links/Links.functions.ts
+++ components/Links/Links.functions.ts
@@ -1,7 +1,7 @@
 export const links = [
   { name: '#Instagram', src: 'https://www.instagram.com/_mirage.gmbh' },
   { name: '#LinkedIn', src: 'https://www.linkedin.com/company/miragegmbh' },
-  { name: '#TikTok', src: '' },
+  { name: '#TikTok', src: 'http://www.tiktok.com/@miragegmbh' },
   { name: '#Email', src: 'mailto:mail@mirage.gmbh' },
   { name: '#Impressum', src: '/impressum' },
 ]
