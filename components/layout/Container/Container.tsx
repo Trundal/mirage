f9918d55c24102430bdde1dd652bbc9b15c8baf3
Korey Powell <korey@vagabonddream.com>
2025-01-10 17:18:19 +0100

    install Sanity CMS

--- components/layout/Container/Container.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './Container.module.css'
-
-type PageContainerTypes = {
-  children: string | JSX.Element | JSX.Element[]
-}
-
-export default function PageContainer({ children }: PageContainerTypes) {
-  return <div className={styles.container}>{children}</div>
-}
